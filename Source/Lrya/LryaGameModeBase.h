2d711be2f81674ff3fddf75c3b09412e60f0e50f
xingge <861312258@qq.com>
2025-09-17 00:41:37 +0800

    项目初始化

---
+++ Source/Lrya/LryaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LryaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LRYA_API ALryaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
